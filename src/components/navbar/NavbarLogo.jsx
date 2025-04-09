cee6d310840eae702357a1615eb0a71ff728c297
Shrihari Eknathe <shriharieknathe999@gmail.com>
2025-04-09 13:37:34 +0530

    minor chnages

--- src/components/navbar/NavbarLogo.jsx
+++ src/components/navbar/NavbarLogo.jsx
@@ -5,7 +5,7 @@ const NavbarLogo = () => {
         Shrihari Eknathe
       </h1>
       <h1 className="text-white font-special font-extrabold text-4xl md:hidden sm:block">
-        AQ
+        SE
       </h1>
     </div>
   );
