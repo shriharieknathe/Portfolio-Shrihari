8ca74969e9df0c5e4063e8aeb56dab3b4c6a8d06
Shrihari Eknathe <shriharieknathe999@gmail.com>
2025-04-09 23:50:27 +0530

    remove directories for images

--- src/components/heroSection/HeroPic.jsx
+++ src/components/heroSection/HeroPic.jsx
@@ -12,7 +12,7 @@ const HeroPic = () => {
       className=" h-full flex items-center justify-center "
     >
       <img
-        src="../../public/images/Profile.png"
+        src="images/Profile.png"
         alt="Shrihari Eknathe"
         className="max-h-[450px] w-auto -translate-y-[50px]"
       />
